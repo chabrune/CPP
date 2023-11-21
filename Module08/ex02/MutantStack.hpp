236b79bfb8472fadfb3b59d7758e036c0a1049f1
Charles Brunet <chabrune@k0r1p7.42mulhouse.fr>
2023-11-21 17:30:21 +0100

    CPP

--- Module08/ex02/MutantStack.hpp
+++ Module08/ex02/MutantStack.hpp
@@ -1,8 +1,28 @@
 #pragma once
 
+#include <stack>
+#include <list>
+#include <iostream>
+#include <deque>
+
 template<typename T>
 class MutantStack : public std::stack<T>
 {
+    public:
     typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+    // typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+    // typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
+    iterator begin(){ return(std::stack<T>::c.begin()); }
+    iterator end(){ return(std::stack<T>::c.end()); }
+    reverse_iterator rbegin() { return(std::stack<T>::c.rbegin()); }
+    reverse_iterator rend() { return(std::stack<T>::c.rend()); }
+    MutantStack() : std::stack<T>() {}
+    MutantStack(const std::stack<T> &src) : std::stack<T>(src) {}
+    ~MutantStack(){}
 };
+
+//Encapsulation de c qui est un std::deque<T> par defaut
+//containeur_type represente le containeur sous jaccent
+//Acces a l'iterateur sous jaccent et en faire un alias pour acceder a begin/end etc
--- Module08/ex02/main.cpp
+++ Module08/ex02/main.cpp
@@ -1,8 +1,47 @@
-#include <iostream>
-#include <list>
+
 #include "MutantStack.hpp"
 
 int main()
 {
-	return 0;
+	{
+		std::cout << "----- STACK -----" << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(10);
+		mstack.push(50);
+		mstack.push(70);
+		mstack.push(90);
+		mstack.pop();
+		mstack.push(42);
+		std::cout << std::boolalpha << "is mstack empty ? : " << mstack.empty() << " - size = " << mstack.size() << std::endl;
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		for(; it != ite; ++it)
+			std::cout << *it << std::endl;
+		std::cout << std::endl <<  "Here with reverse iterator :" << std::endl;
+		MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+		MutantStack<int>::reverse_iterator rite = mstack.rend();
+		for(; rit != rite; ++rit)
+			std::cout << *rit << std::endl;
+		std::cout << std::endl;
+	}
+	{
+		std::cout << "----- LIST -----" << std::endl;
+		std::list<int> lst;
+		lst.push_back(10);
+		lst.push_back(50);
+		lst.push_back(70);
+		lst.push_back(90);
+		lst.pop_back();
+		lst.push_back(42);
+		std::cout << std::boolalpha << "is lst empty ? : " << lst.empty() << " - size = " << lst.size() << std::endl;
+		std::list<int>::iterator it = lst.begin();
+		std::list<int>::iterator ite = lst.end();
+		for(; it != ite; ++it)
+			std::cout << *it << std::endl;
+		std::cout << std::endl <<  "Here with reverse iterator :" << std::endl;
+		std::list<int>::reverse_iterator rit = lst.rbegin();
+		std::list<int>::reverse_iterator rite = lst.rend();
+		for(; rit != rite; ++rit)
+			std::cout << *rit << std::endl;
+	}
 }
